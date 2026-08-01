5ddc2af5a1d3ce2bce900c32e444963b05c7ee97
leonidaspantelis01 <leonidaspantelis01@gmail.com>
2026-08-01 19:25:16 +0300

    Update 03_Product_Analysis.sql

--- SQL/03_Product_Analysis.sql
+++ SQL/03_Product_Analysis.sql
@@ -38,4 +38,10 @@ Top 10 Products by Quantity Sold
 
 ============================================================
 */
-
+SELECT
+    `Product Name`,
+    SUM(Quantity) AS Total_Quantity
+FROM superstore
+GROUP BY `Product Name`
+ORDER BY Total_Quantity DESC
+LIMIT 10;
