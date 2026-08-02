f8cb3d771957192e32579eaec28357299f52d000
leonidaspantelis01 <leonidaspantelis01@gmail.com>
2026-08-02 14:29:17 +0300

    Inside Q11

--- SQL/04_Order_Analysis.sql
+++ SQL/04_Order_Analysis.sql
@@ -13,3 +13,19 @@ FROM superstore
 GROUP BY `Customer Name`
 ORDER BY Total_Orders DESC
 LIMIT 10;
+/*
+==========================================================
+Business Question 11
+
+Average Order Value (AOV)
+==========================================================
+*/
+
+SELECT
+    ROUND(SUM(Sales), 2) AS Total_Sales,
+    COUNT(DISTINCT `Order ID`) AS Total_Orders,
+    ROUND(
+        SUM(Sales) / COUNT(DISTINCT `Order ID`),
+        2
+    ) AS Average_Order_Value
+FROM superstore;
